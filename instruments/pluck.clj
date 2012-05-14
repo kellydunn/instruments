11c7f49415595067c61491396f6493111d4e2588
kellydunn <defaultstring@gmail.com>
2012-05-13 17:54:43 -0700

    [git][filesys] Reorganizing

---
+++ instruments/pluck.clj
@@ -0,0 +1,6 @@
+(definst pick [freq 220]
+  (let [src (+ (pluck (* (pink-noise) (env-gen (perc 0.001 0.5) :action FREE)) 1 1 (/ 4 freq))
+               (sin-osc (/ freq 2))
+               (sin-osc (/ freq 3)))
+        filt (env-gen (perc 0.001 2) :action FREE)]
+    (lpf (* 0.5 (* src filt)) 1250)))
