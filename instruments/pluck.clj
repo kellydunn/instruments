48200e2de98b7a6019784d138457a8b60fc6c393
kellydunn <defaultstring@gmail.com>
2012-05-13 18:02:21 -0700

    [src] Keeping note of what needs to change for dynamic scale biddness.

--- instruments/pluck.clj
+++ instruments/pluck.clj
@@ -1,5 +1,6 @@
-(definst pick [freq 220]
-  (let [src (+ (pluck (* (pink-noise) (env-gen (perc 0.001 0.5) :action FREE)) 1 1 (/ 4 freq))
+(definst pick [note 20]
+  (let [freq (midicps note)
+        src (+ (pluck (* (pink-noise) (env-gen (perc 0.001 0.5) :action FREE)) 1 1 (/ 4 freq))
                (sin-osc (/ freq 2))
                (sin-osc (/ freq 3)))
         filt (env-gen (perc 0.001 2) :action FREE)]
