b75e39bd005684014b3aad019bfc3e80b41eff02
kellydunn <defaultstring@gmail.com>
2012-06-08 17:09:03 -0700

    [src] Adding some detuned triangle for some warmth

--- instruments/heavy_bass.clj
+++ instruments/heavy_bass.clj
@@ -1,7 +1,10 @@
 (definst pick [note 20]
   (let [freq (midicps note)
         src (+ (pluck (* (pink-noise) (env-gen (perc 0.001 0.5) :action FREE)) 1 1 (/ 4 freq))
+               (lf-tri (* freq 0.025))
                (sin-osc (/ freq 2))
                (sin-osc (/ freq 3)))
         filt (env-gen (perc 0.001 2) :action FREE)]
-    (lpf (* 0.5 (* src filt)) 1250)))
+    (lpf (* 0.5 (* src filt)) 1250)))y
+
+(pick 60)
